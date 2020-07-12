c4b587674d78f6d131bfb47ca8768cdacbbaedaa
Jian <450650800@qq.com>
2020-07-12 19:09:42 +0800

    feat : 提交作业 零移动.

---
+++ Week_01/零移动/MoveZeroes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MoveZeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testData = new int[] { 0, 1, 0, 3, 12 };
+            MoveZeroes(testData);
+            String s = String.Join(",", testData);
+            Console.WriteLine(s);
+            Console.WriteLine("任意键结束");
+            Console.ReadKey();
+
+        }
+
+        /// <summary>
+        /// 零移动
+        /// </summary>
+        /// <param name="nums">数组源数据</param>
+        public static void MoveZeroes(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return;
+            int j = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[j] = nums[i];
+                    if (i != j)
+                    {
+                        nums[i] = 0;
+                    }
+                    j++;
+                }
+            }
+
+
+        }
+
+    }
+}
