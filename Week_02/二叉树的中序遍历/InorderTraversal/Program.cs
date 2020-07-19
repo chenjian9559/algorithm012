b7be82c11b409c3615555475ac4a7065c23452de
Jian <450650800@qq.com>
2020-07-19 23:39:49 +0800

    feat: 二叉数的中序遍历和字母异位词分组。

---
+++ Week_02/二叉树的中序遍历/InorderTraversal/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InorderTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+        #region 递归形式
+        public static IList<int> InorderTraversal(TreeNode root)
+        {
+            IList<int> TraversedNode = new List<int>();
+            Helper(root, TraversedNode);
+            return TraversedNode;
+        }
+
+        public static void Helper(TreeNode root, IList<int> TraverserNode)
+        {
+            if (root != null)
+            {
+                if (root.left != null)
+                {
+                    Helper(root.left, TraverserNode);
+                }
+                TraverserNode.Add(root.val);
+                if (root.right != null)
+                {
+                    Helper(root.right, TraverserNode);
+                }
+            }
+        }
+
+        #endregion 
+
+
+    }
+
+    /// <summary>
+    ///  Definition for a binary tree node.
+    /// </summary>
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode() { }
+        public TreeNode(int x) { val = x; }
+    }
+
+}
---
+++ Week_02/字母异位词分组/GroupAnagrams/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Schema;
+
+namespace GroupAnagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] testData = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+            Console.WriteLine($"测试数据{String.Join(",", testData)}");
+            var reuslt = GroupAnagrams(testData);
+            Console.WriteLine($"--------GroupAnagrams  分组后结果：");
+            foreach (var item in reuslt)
+            {
+                Console.WriteLine($"{String.Join(",", item)}");
+            }
+            Console.WriteLine($"--------GroupAnagrams  The End");
+
+
+            var reuslt2 = GroupAnagrams2(testData);
+            Console.WriteLine($"--------GroupAnagrams2  分组后结果：");
+            foreach (var item in reuslt2)
+            {
+                Console.WriteLine($"{String.Join(",", item)}");
+            }
+            Console.WriteLine($"--------GroupAnagrams2  The End");
+
+
+            Console.WriteLine($"按任意键结束！");
+            Console.ReadKey();
+
+        }
+
+        public static IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            Dictionary<string, IList<string>> dic = new Dictionary<string, IList<string>>();
+            foreach (var item in strs)
+            {
+                int[] characters = new int[26];
+                for (int i = 0; i < 26; i++)
+                {
+                    characters[i] = 0;
+                }
+                for (int j = 0; j < item.Length; j++)
+                {
+                    var ascii = System.Text.Encoding.ASCII.GetBytes(item.Substring(j, 1));
+                    characters[ascii[0] - 97]++;
+                }
+                StringBuilder key = new StringBuilder();
+                for (int z = 0; z < characters.Length; z++)
+                {
+                    key.Append(characters[z].ToString());
+                }
+                if (dic.ContainsKey(key.ToString()))
+                {
+                    dic[key.ToString()].Add(item);
+                }
+                else
+                {
+                    List<string> demo1 = new List<string>();
+                    demo1.Add(item);
+                    dic.Add(key.ToString(), demo1);
+                }
+            }
+            return dic.Values.ToList();
+        }
+
+
+        public static IList<IList<string>> GroupAnagrams2(string[] strs)
+        {
+            var res = new List<IList<string>>();
+            var grp = new Dictionary<string, IList<string>>();
+            // 遍历每一个字符串
+            foreach (var str in strs)
+            {
+                // Concat:将指定字符串连接到此字符串的结尾
+                // OrderBy 将char升序排列
+                // rt是排序过的字母 eat -> aet
+                string rt = String.Concat(str.OrderBy(ch => ch));
+
+                // 放入字典中
+                if (grp.ContainsKey(rt))
+                {
+                    grp[rt].Add(str);
+                }
+                else
+                {
+                    grp[rt] = new List<string> { str };
+                }
+            }
+            foreach (var key in grp.Keys)
+            {
+                res.Add(grp[key]);
+            }
+            return res;
+        }
+
+
+    }
+
+}
