9fa52e70fa907ddc4009d13a194a929e8ca5e243
Jian <450650800@qq.com>
2020-07-26 23:50:46 +0800

    feat：全排列和组合

---
+++ Week_03/全排列/Permute/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Permute
+{
+    /// <summary>
+    /// 给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             * 示例:
+             *    输入: [1,2,3]
+             *    输出:
+             *    [
+             *      [1,2,3],
+             *      [1,3,2],
+             *      [2,1,3],
+             *      [2,3,1],
+             *      [3,1,2],
+             *      [3,2,1]
+             *    ]
+             **/
+            int[] testData = new int[] { 1, 2, 3 };
+            var reuslt = Permute(testData);
+            Console.WriteLine($"--------结果：");
+            foreach (var item in reuslt)
+            {
+                Console.WriteLine($"{String.Join(",", item)}");
+            }
+            Console.WriteLine($"--------The End");
+
+            Console.WriteLine($"按任意键结束！");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static IList<IList<int>> Permute(int[] nums)
+        {
+
+            IList<IList<int>> res = new List<IList<int>>(); //存放所有解
+            LinkedList<int> track = new LinkedList<int>(); //存放临时解，用了链表
+            backrack(nums, track, res);
+            return res;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="track"></param>
+        /// <param name="res"></param>
+        public static void backrack(int[] nums, LinkedList<int> track, IList<IList<int>> res)
+        {
+            if (track.Count == nums.Length) //判断临时解的个数是否等于num的个数
+            {
+                res.Add(new List<int>(track)); //是就直接添加并返回
+                return;
+            }
+            for (int i = 0; i < nums.Length; i++)  //从第一个数开始
+            {
+                if (track.Contains(nums[i]))  //临时链表包涵这个数，说明就是这个数已经选择过作为结点的了
+                {
+                    continue; ;
+                }
+                track.AddLast(nums[i]); //不包含这个数，就把这个数插进链表最后
+                backrack(nums, track, res);//继续下一个
+                track.RemoveLast();//为了维护临时链表的正确，离开这层循环，都要删除这层选择的数
+            }
+        }
+
+    }
+}
---
+++ Week_03/组合/Combine/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Combine
+{
+    /// <summary>
+    /// 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            /**
+             * 示例:
+             * 
+             * 输入: n = 4, k = 2
+             * 输出:
+             * [
+             *   [2,4],
+             *   [3,4],
+             *   [2,3],
+             *   [1,2],
+             *   [1,3],
+             *   [1,4],
+             * ]
+             */
+            // n = 4 ,k = 2
+            var result = Combine(4, 2);
+
+
+
+
+            Console.WriteLine($"--------结果：");
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{String.Join(",", item)}");
+            }
+            Console.WriteLine($"--------The End");
+            Console.WriteLine($"按任意键结束！");
+            Console.ReadKey();
+        }
+
+        public static IList<IList<int>> Combine(int n, int k)
+        {
+            if (n < 1 || k < 1) return new List<IList<int>>();
+            List<IList<int>> results = new List<IList<int>>();
+            List<int> currentList = new List<int>();
+            BuildNumberList(1, n, k, ref currentList, ref results);
+            return results;
+        }
+
+        private static void BuildNumberList(int start, int end, int length, ref List<int> currentList, ref List<IList<int>> results)
+        {
+            if (currentList.Count == length)
+            {
+                results.Add(new List<int>(currentList));
+                return;
+            }
+
+            for (int index = start; index <= end; index++)
+            {
+                currentList.Add(index);
+                BuildNumberList(index + 1, end, length, ref currentList, ref results);
+                currentList.RemoveAt(currentList.Count - 1);
+            }
+        }
+    }
+}
