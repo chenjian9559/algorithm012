cbd76f3fddd8301841c2effb26693ba3d55514bb
Jian <450650800@qq.com>
2020-08-24 00:04:30 +0800

    feat：22. 括号生成、70. 爬楼梯

---
+++ Week_07/22. 括号生成/GenerateParenthesis/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenerateParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+
+示例：
+    输入：n = 3
+    输出：[
+            '((()))',
+            '(()())',
+            '(())()',
+            '()(())',
+            '()()()'
+          ]");
+
+            int testData = 3;
+            Console.Write($"--------测试用例：");
+            Console.WriteLine($"{testData}");
+
+            var result = GenerateParenthesis(testData); // 算法实现
+
+            Console.WriteLine($"--------结果：");
+            Console.WriteLine($"{string.Join("\n",result)}");
+            Console.WriteLine($"--------The End");
+
+            Console.WriteLine($"按任意键结束！");
+            Console.ReadKey();
+        }
+        public  static IList<string> GenerateParenthesis(int n)
+        {
+            List<string> result = new List<string>();
+            StringBuilder sb = new StringBuilder();
+            void GetNext(int l, int r)
+            {
+                if (l > r && l == n)
+                {
+                    sb.Append(')');
+                    GetNext(l, r + 1);
+                }
+                else if (l > r && l < n)
+                {
+                    sb.Append('(');
+                    GetNext(l + 1, r);
+                    sb.Append(')');
+                    GetNext(l, r + 1);
+                }
+                else if (l == r && l < n)
+                {
+                    sb.Append('(');
+                    GetNext(l + 1, r);
+                }
+                else if (l == r && l == n)
+                {
+                    result.Add(sb.ToString());
+                }
+                sb.Remove(sb.Length - 1, 1);
+            }
+            sb.Append('(');
+            GetNext(1, 0);
+            return result;
+        }
+    }
+}
---
+++ Week_07/70. 爬楼梯/ClimbStairs/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClimbStairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+注意：给定 n 是一个正整数。
+
+示例 1：
+    输入： 2
+    输出： 2
+解释： 有两种方法可以爬到楼顶。
+    1.  1 阶 + 1 阶
+    2.  2 阶
+示例 2：
+    输入： 3
+    输出： 3
+    解释： 有三种方法可以爬到楼顶。
+    1.  1 阶 + 1 阶 + 1 阶
+    2.  1 阶 + 2 阶
+    3.  2 阶 + 1 阶");
+
+            int testData = 40;
+            Console.Write($"--------测试用例：");
+            Console.WriteLine($"{testData}");
+
+            var result = ClimbStairs(testData); // 算法实现
+
+            Console.WriteLine($"--------结果：");
+            Console.WriteLine($"{result}");
+            Console.WriteLine($"--------The End");
+
+            Console.WriteLine($"按任意键结束！");
+            Console.ReadKey();
+
+        }
+
+        public static int ClimbStairs(int n)
+        {
+            //return fib1(n);
+            return Fib2(n, 1, 2);
+            //return Fibo3(n);
+
+        }
+
+        public static int fib1(int n)
+        {
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+            int[] fib1 = new int[n];
+            fib1[0] = 1;
+            fib1[1] = 2;
+            for (int i = 2; i < n; i++)
+            {
+                fib1[i] = fib1[i - 2] + fib1[i - 1];
+            }
+            return fib1[n - 1];
+        }
+
+        public static int Fib2(int n, int first, int second)
+        {
+            if (n <= 1)
+            {
+                return first;
+            }
+            else
+            {
+                return Fib2(n - 1, second, first + second);
+            }
+        }
+
+        public static int Fibo3(int n)
+        {
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+            int first = 1;
+            int second = 2;
+            int third = 3;
+            for (int i = 3; i <= n; i++)
+            {
+                third = first + second;
+                first = second;
+                second = third;
+            }
+            return third;
+        }
+    }
+}
