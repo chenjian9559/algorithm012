82517453d63fc81170cf3f19913b97ef5d940118
Jian <450650800@qq.com>
2020-09-13 20:43:39 +0800

    feat:   64.最小路径和、32.最长有效括号 and 总结

---
+++ Week_06/32. 最长有效括号/LongestValidParentheses/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LongestValidParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+示例 1:
+    输入: '(()'
+    输出: 2
+    解释: 最长有效括号子串为 '()'
+示例 2:
+    输入: ')()())'
+    输出: 4
+    解释: 最长有效括号子串为 '()()'");
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("以下是该程序实现执行的结果：");
+
+           string testData = ")()())";
+            Console.Write($"--------测试用例：");
+            Console.WriteLine($"{testData}");
+
+            var result = LongestValidParentheses(testData); // 算法实现
+
+            Console.WriteLine($"--------结果：");
+            Console.WriteLine($"{result}");
+            Console.WriteLine($"--------The End");
+
+            Console.WriteLine($"按任意键结束！");
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int LongestValidParentheses(string s)
+        {
+            Stack<int> leftC = new Stack<int>();
+            var step = 0;
+            var ans = 0;
+            leftC.Push(-1);
+            while (step < s.Length)
+            {
+                if (s[step] == '(')
+                {
+                    leftC.Push(step);
+                }
+                else if (s[step] == ')')
+                {
+                    leftC.Pop();
+                    if (leftC.Count > 0)
+                    {
+                        ans = Math.Max(ans, step - leftC.Peek());
+                    }
+                    else
+                    {
+                        leftC.Push(step);
+                    }
+                }
+                step++;
+            }
+            return ans;
+        }
+    }
+}
---
+++ Week_06/64. 最小路径和/MinPathSum/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinPathSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+说明：每次只能向下或者向右移动一步。
+
+示例:
+	输入:
+    [
+        [1,3,1],
+        [1,5,1],
+        [4,2,1]
+    ]
+	输出: 7
+	解释: 因为路径 1→3→1→1→1 的总和最小。");
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("以下是该程序实现执行的结果：");
+
+            int[][] testData = new int[][] {
+                new int[]{1, 3, 1},
+               new int[]{ 1, 5, 1},
+                new int[]{ 4, 2, 1}
+            };
+            Console.WriteLine($"--------测试用例：");
+            Console.WriteLine($"[{String.Join(",", testData[0])}]");
+            Console.WriteLine($"[{String.Join(",", testData[1])}]");
+            Console.WriteLine($"[{String.Join(",", testData[2])}]");
+
+            var result = MinPathSum(testData); // 算法实现
+
+            Console.WriteLine($"--------结果：");
+            Console.WriteLine($"{result}");
+            Console.WriteLine($"--------The End");
+
+            Console.WriteLine($"按任意键结束！");
+            Console.ReadKey();
+
+        }
+
+
+        public static int MinPathSum(int[][] grid)
+        {
+            if (grid == null || grid.Length == 0)
+            {
+                return 0;
+            }
+            int m = grid.Length;
+            int n = grid[0].Length;
+            //遍历grid数组，动态规划计算每个格子的最小值
+            for (int x = 0; x < m; x++)
+            {
+                for (int y = 0; y < n; y++)
+                {
+                    if (x == 0 && y == 0) { continue; }
+                    int curMin = -1;
+                    //从当前格子的上边和左边的格子里选出最小值并与当前格相加
+                    if (x - 1 >= 0 && y - 1 >= 0)
+                    {
+                        curMin = Math.Min(grid[x - 1][y], grid[x][y - 1]);
+                    }
+                    else
+                    {
+                        curMin = x - 1 >= 0 ? grid[x - 1][y] : grid[x][y - 1];
+                    }
+                    grid[x][y] = curMin + grid[x][y];
+                }
+            }
+            //输出终点计算出的值即为路径最小值
+            return grid[m - 1][n - 1];
+        }
+    }
+}
